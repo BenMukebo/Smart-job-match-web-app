ee8cc8ed3cd38757c4151a3715249b9a4bc44b53
BenMukebo <l.benkasmukebo7@gmail.com>
2025-05-09 20:03:24 +0200

    Wrap the GeminiContextProvider to the add and add a logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import GeminiContextProvider from './contexts/useGemininContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <GeminiContextProvider>
     <App />
-  </StrictMode>,
+  </GeminiContextProvider>,
 )
