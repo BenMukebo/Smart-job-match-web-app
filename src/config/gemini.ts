90d9838bedea55912252aadae68a1c05349f7b1e
BenMukebo <l.benkasmukebo7@gmail.com>
2025-05-10 00:47:04 +0200

    Update the shadcn colors and update the mainSummary and add the SheetTrigger

--- src/config/gemini.ts
+++ src/config/gemini.ts
@@ -6,14 +6,14 @@ import {
   GoogleGenAI,
 } from '@google/genai';
 
-async function mainSummary(prompt: string) {
+async function mainSummary(prompt: string, onChunk?: (chunk: string) => void) {
   const ai = new GoogleGenAI({
     apiKey: import.meta.env.VITE_GEMINI_API_KEY,
   });
   const config = {
     responseMimeType: 'text/plain',
   };
-  // const model = 'gemini-2.5-pro-preview-05-06'; 
+  // const model = 'gemini-2.5-pro-preview-05-06';
   const model = 'gemini-2.5-pro-exp-03-25';
   const contents = [
     {
@@ -32,9 +32,11 @@ async function mainSummary(prompt: string) {
     contents,
   });
   for await (const chunk of response) {
+    if (onChunk) {
+      onChunk(chunk.text || "");
+    }
     console.log("chunk.text", chunk.text);
   }
 }
 
-// mainSummary();
 export default mainSummary;
