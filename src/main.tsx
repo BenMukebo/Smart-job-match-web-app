d8cac2dcff6ff54211aa857402ad95db22563529
BenMukebo <l.benkasmukebo7@gmail.com>
2025-05-23 16:07:14 +0200

    Refactor App component to use React Router for navigation and lazy loading of routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import GeminiContextProvider from './contexts/useGemininContext.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <GeminiContextProvider>
-    <App />
-  </GeminiContextProvider>,
-)
+import "./index.css";
+import App from "./App.tsx";
+import GeminiContextProvider from "./contexts/useGemininContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <GeminiContextProvider>
+      <App />
+    </GeminiContextProvider>
+  </BrowserRouter>
+);
